e6cb2b16e1f8aaefdff5fbbe7f3b042fea99ea9a
Fastre <61952761+IamFastre@users.noreply.github.com>
2025-10-09 17:26:08 +0300

    hello world with checkbox element

---
+++ src/lib/basic/Checkbox.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+  import { Check, Circle } from "@lucide/svelte";
+  import type { HTMLButtonAttributes } from "svelte/elements";
+  import { blur } from "svelte/transition";
+
+  interface Props {
+    active?: boolean;
+    style: 'hollow' | 'check' | 'radio' | 'custom';
+  }
+
+  let { active = $bindable(false), style, ...rest }:Props & HTMLButtonAttributes = $props();
+</script>
+
+{#snippet active_symbol()}
+  <div class={["active-symbol", style]} transition:blur={{ duration:300 }}>
+    {#if      style === 'hollow'}
+      {null}
+    {:else if style === 'check'}
+      <Check />
+    {:else if style === 'radio'}
+      <Circle />
+    {:else if style === 'custom'}
+      {null}
+    {/if}
+  </div>
+{/snippet}
+
+<button
+  data-variant={active ? "main" : "hollow"}
+  {...rest}
+  class={["titchy", "checkbox", `style-${style}`, rest.class]}
+  class:active
+  onclick={rest.onclick ?? (() => active =! active)}
+>
+  {#if style === 'custom'}
+    {@render rest.children?.()}
+  {:else if active}
+    {@render active_symbol()}
+  {/if}
+</button>
+
+<style lang="scss">
+  @use "@/others/utils.scss" as *;
+
+  $size:  var(--checkbox-size, 30px);
+  $color: var(--checkbox-color, C(accent));
+
+  .titchy.checkbox {
+    @include size($size, $both:true);
+
+    font-weight: 900;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid C(secondary, 2.5%);
+    border-radius: V(radius-1);
+    overflow: hidden;
+
+    &.active {
+      color: $color;
+      background-color: set-alpha($color, 10%);
+      &:hover { background-color: set-alpha($color, 20%); }
+    }
+
+    &:not(.active) {
+      background-color: C(primary);
+      &:hover { background-color: C(tertiary); }
+    }
+
+    .active-symbol {
+      align-items: center;
+      justify-content: center;
+
+      :global(svg) {
+        color: $color;
+        stroke-width: 4px;
+      }
+
+      &.hollow {
+        @include size(calc($size / 1.5), $both:true);
+        border-radius: calc(V(radius-1) / 1.5);
+        background-color: $color;
+      }
+
+      &.check :global(svg) {
+        @include size(calc($size / 1.5), $both:true);
+      }
+
+      &.radio :global(svg) {
+        @include size(calc($size / 1.75), $both:true);
+      }
+    }
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  import "$lib/styles/global.css";
+  import { Checkbox } from "$lib/basic/index.js";
+</script>
+
+<div class="items">
+  <div class="item">
+    <h1>Checkbox:</h1>
+    <div class="showcase-grid">
+      <div class="row-title">
+        <span>Active</span>
+      </div>
+      <div class="showcase">
+        <Checkbox active style="hollow" />
+        <Checkbox active style="check" />
+        <Checkbox active style="radio" />
+        <Checkbox active style="custom">Y</Checkbox>
+      </div>
+
+      <div class="row-title">
+        <span>Inactive</span>
+      </div>
+      <div class="showcase">
+        <Checkbox style="hollow" />
+        <Checkbox style="check" />
+        <Checkbox style="radio" />
+        <Checkbox style="custom">N</Checkbox>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  @use "@/others/utils.scss" as *;
+
+  .items {
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .item {
+    background-color: #000;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    padding: 15px;
+    border: 2px solid #1f1f1f;
+    border-radius: 10px;
+
+    h1 { margin: 0; }
+
+    .showcase-grid{
+      display: grid;
+      grid-template-columns: max-content 1fr;
+      gap: 10px;
+
+      .row-title {
+        color: C(secondary, 80%);
+        align-items: end;
+        justify-content: center;
+      }
+
+      .showcase {
+        gap: 15px;
+        flex-direction: row;
+        align-items: center;
+
+        :global(>:last-child) {
+          --checkbox-color: cyan;
+          font-family: monospace;
+        }
+      }
+    }
+  }
+</style>
