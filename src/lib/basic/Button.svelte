30375c2f9678c395a532cb4ade8c99e88d46d8bf
Fastre <61952761+IamFastre@users.noreply.github.com>
2025-10-11 03:19:31 +0300

    feat: added a primitive button component

---
+++ src/lib/basic/Button.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import type { HTMLButtonAttributes } from "svelte/elements";
+
+  interface Props {
+    variant?: 'primary' | 'secondary';
+  }
+
+  let { variant = 'primary', ...rest }:Props & HTMLButtonAttributes = $props();
+</script>
+
+<button
+  {...rest}
+  class={["titchy", "button", variant, rest.class]}
+>
+  {@render rest.children?.()}
+</button>
+
+<style lang="scss">
+  @use "@/others/utils.scss" as *;
+
+  $accent-color: var(--button-accent-color, C(accent));
+  $text-color: var(--button-text-color, C(primary));
+
+  :global
+  .titchy.button {
+    cursor: pointer;
+    padding: 5px 7.5px;
+
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 7.5px;
+
+    color: $text-color;
+    background-color: $accent-color;
+    border-radius: V(radius-1);
+
+    &:hover {
+      transform: scale(1.05);
+      opacity: 1;
+    }
+
+    &:active {
+      transform: scale(0.95);
+      opacity: 0.8;
+    }
+
+    &.primary {
+      padding: 7px 9.5px;
+    }
+
+    &.secondary {
+      color: $accent-color;
+      background-color: $text-color;
+
+      border: 2px solid $accent-color;
+    }
+  }
+</style>
--- src/lib/basic/index.ts
+++ src/lib/basic/index.ts
@@ -1,2 +1,3 @@
+export { default as Button } from "./Button.svelte";
 export { default as Checkbox } from "./Checkbox.svelte";
 export { default as Loading } from "./Loading.svelte";
